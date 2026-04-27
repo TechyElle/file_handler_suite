e7d858b566d58f558b52b39b3ef42818e1c17922
Cielle Mae Peñamora <pciellemae@gmail.com>
2026-04-27 19:48:05 +0800

    chore: add module docstring and imports

---
+++ file_handlers/number_separator.py
@@ -0,0 +1,8 @@
+"""Module for separating even and odd numbers from a file.
+
+Implements Problem P-1: Read 20 integers from a text file and create
+two separate files containing even and odd numbers respectively.
+"""
+
+from typing import List, Tuple
+from file_handler_base import FileHandler
