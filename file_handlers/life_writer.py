195604ed91f140d4a2d6155d11132b1fa6556bcd
Cielle Mae Peñamora <pciellemae@gmail.com>
2026-04-27 20:12:10 +0800

    chore: add module docstring and imports

---
+++ file_handlers/life_writer.py
@@ -0,0 +1,9 @@
+"""Module for interactively writing lines to a text file.
+
+Implements Problem P-3: Write multiple lines of text into mylife.txt
+through an interactive prompt-based interface.
+"""
+
+from typing import List
+
+from file_handler_base import FileHandler
